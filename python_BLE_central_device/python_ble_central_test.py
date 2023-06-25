989b4dfa4f6bd7d8127eab67622bed4653097ff3
Bingjian Huang <huangbj16@gmail.com>
2023-06-25 15:09:32 -0400

    small updates

--- python_BLE_central_device/python_ble_central_test.py
+++ python_BLE_central_device/python_ble_central_test.py
@@ -7,12 +7,13 @@ MOTOR_UUID = 'f22535de-5375-44bd-8ca9-d0ea9ff9e410'
 async def setMotor(client):
     while True:
         motor_addr = int(input('what is the next motor you want to control?'))
+        duty = int(input('duty?'))
         start_or_stop = int(input('1 for start and 0 for stop?'))
         # duty = int(input('0-3 for duty?'))
         command = {
             'addr':motor_addr,
             'mode':start_or_stop,
-            'duty':3, # default
+            'duty':duty, # default
             'freq':2, # default
             'wave':1, # default
         }
--- python_BLE_central_device/quick_generator.py
+++ python_BLE_central_device/quick_generator.py
@@ -1,17 +1,17 @@
 import time
 
-def precise_sleep(duration):
-    start = time.perf_counter()
-    end = start + duration
+# def precise_sleep(duration):
+#     start = time.perf_counter()
+#     end = start + duration
 
-    while time.perf_counter() < end:
-        pass
+#     while time.perf_counter() < end:
+#         pass
 
-    actual_sleep_duration = time.perf_counter() - start
-    print(f"{start}, {time.perf_counter()}, Actual sleep duration: {actual_sleep_duration} seconds")
+#     actual_sleep_duration = time.perf_counter() - start
+#     print(f"{start}, {time.perf_counter()}, Actual sleep duration: {actual_sleep_duration} seconds")
 
-for i in range(10):
-    precise_sleep(0.1)
+# for i in range(10):
+#     precise_sleep(0.1)
 
 
 # import json
@@ -31,3 +31,93 @@ for i in range(10):
 #     for command in commands:
 #         json.dump(command, file)
 #         file.write("\n")
+
+
+import json
+import numpy as np
+
+commands = []
+
+### no phantom
+
+# duration  = 0.4
+# vib_step = 1.0
+# motor_num = 5
+# start_time = 1.0
+
+# for i in range(1, motor_num+1):
+#     commands.append({"time":round(start_time+i*vib_step, 2), "addr":2*i-1, "mode":1, "duty":3, "freq":2, "wave":1})
+#     commands.append({"time":round(start_time+i*vib_step+duration, 2), "addr":2*i-1, "mode":0, "duty":3, "freq":2, "wave":1})
+
+# commands.append({"time":0, "addr":0, "mode":1, "duty":3, "freq":3, "wave":3})
+# commands.append({"time":round(start_time+duration*motor_num+3.0, 2), "addr":0, "mode":0, "duty":3, "freq":2, "wave":1})
+
+
+### linear phantom sensation
+
+# start_time = 6.0
+# intensity_level = 4
+# duration = 0.1
+# motor_num = 5
+# for i in range(1, motor_num+1):
+#     motor_peak_time = start_time + duration * intensity_level * i
+#     for j in range(-intensity_level+1, intensity_level):
+#         commands.append({"time":round(motor_peak_time+j*duration, 2), "addr":2*i-1, "mode":1, "duty":min(4-abs(j), 3), "freq":2, "wave":1})
+#     commands.append({"time":round(motor_peak_time+intensity_level*duration, 2), "addr":2*i-1, "mode":0, "duty":3, "freq":2, "wave":1})
+
+# for i in range(1, 2*motor_num+1):
+#     commands.append({"time":start_time+duration*intensity_level*motor_num+3.0, "addr":i, "mode":0, "duty":3, "freq":2, "wave":1})
+
+# commands.append({"time":0, "addr":0, "mode":1, "duty":3, "freq":3, "wave":3})
+# commands.append({"time":start_time+duration*intensity_level*motor_num+3.0, "addr":0, "mode":0, "duty":3, "freq":2, "wave":1})
+
+### funneling illusion
+
+# start_time = 10.0
+# duration = 0.4
+# vib_step = 1.0
+# motor_num = 5
+# for i in range(1, motor_num+1):
+#     commands.append({"time":round(start_time+i*vib_step, 2), "addr":2*i-1, "mode":1, "duty":3, "freq":2, "wave":1})
+#     commands.append({"time":round(start_time+i*vib_step, 2), "addr":2*i+1, "mode":1, "duty":3, "freq":2, "wave":1})
+#     commands.append({"time":round(start_time+i*vib_step+duration, 2), "addr":2*i-1, "mode":0, "duty":3, "freq":2, "wave":1})
+#     commands.append({"time":round(start_time+i*vib_step+duration, 2), "addr":2*i+1, "mode":0, "duty":3, "freq":2, "wave":1})
+
+# commands.append({"time":0, "addr":0, "mode":1, "duty":3, "freq":3, "wave":3})
+# commands.append({"time":start_time+vib_step*motor_num+3.0, "addr":0, "mode":0, "duty":3, "freq":2, "wave":1})
+
+### cutaneous rabbit
+
+start_time = 0.0
+vib_step = 5.0
+motor_num = 5
+for i in range(1, motor_num+1):
+    # normal
+    commands.append({"time":round(start_time+i*vib_step+0.00, 2), "addr":i, "mode":1, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+0.20, 2), "addr":i, "mode":0, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+0.20, 2), "addr":i+1, "mode":1, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+0.40, 2), "addr":i+1, "mode":0, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+0.40, 2), "addr":i+2, "mode":1, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+0.60, 2), "addr":i+2, "mode":0, "duty":3, "freq":2, "wave":1})
+    # illusion
+    commands.append({"time":round(start_time+i*vib_step+2.00, 2), "addr":i, "mode":1, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+2.20, 2), "addr":i, "mode":0, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+2.20, 2), "addr":i, "mode":1, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+2.30, 2), "addr":i, "mode":0, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+2.30, 2), "addr":i+2, "mode":1, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+2.40, 2), "addr":i+2, "mode":0, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+2.40, 2), "addr":i+2, "mode":1, "duty":3, "freq":2, "wave":1})
+    commands.append({"time":round(start_time+i*vib_step+2.60, 2), "addr":i+2, "mode":0, "duty":3, "freq":2, "wave":1})
+    
+
+commands.append({"time":0, "addr":0, "mode":1, "duty":3, "freq":3, "wave":3})
+commands.append({"time":start_time+vib_step*motor_num+vib_step+3.0, "addr":0, "mode":0, "duty":3, "freq":2, "wave":1})
+
+commands.sort(key=lambda x: x['addr'])
+commands.sort(key=lambda x: x['time'])
+
+file_path = 'commands_cutaneous.json'
+with open(file_path, "w") as file:
+    for command in commands:
+        json.dump(command, file)
+        file.write("\n")
